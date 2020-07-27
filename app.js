9206010e3fb5240895360bff7a8a94c205801f6d
myhkas <mmyhkas@gmail.com>
2020-07-26 23:05:48 -0700

    added about me and portfolio

--- app.js
+++ app.js
@@ -3,4 +3,22 @@ $(document).ready(function () {
   $(".carousel.carousel-slider").carousel({
     fullWidth: true,
   });
+
+  $(document).on("click", "#portfolioBtn", () => {
+    $("html, body").animate(
+      {
+        scrollTop: $("#portfolio").offset().top,
+      },
+      1500
+    );
+  });
+
+  $(document).on("click", "#aboutBtn", () => {
+    $("html, body").animate(
+      {
+        scrollTop: $("#aboutMe").offset().top,
+      },
+      1500
+    );
+  });
 });
